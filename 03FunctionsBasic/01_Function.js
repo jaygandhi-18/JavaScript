38f6987a7a8260cb247c21234fe2f24b41daa1f5
Jay Gandhi <Jaygandhi813@gmail.com>
2025-12-11 14:20:59 +0530

    update 01function file

--- 03FunctionsBasic/01_Function.js
+++ 03FunctionsBasic/01_Function.js
@@ -4,10 +4,19 @@ function myName() {
     console.log("y");
     return
 }
-
 // myName()
 
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+
+
 function sumof(num1, num2) {
+    // let result = number1 + number2
+    // return result
     return num1 + num2
 }
 
@@ -25,3 +34,35 @@ function userLoginChack(user){
 
 const output = userLoginChack("Jay")
 console.log(output);
+
+
+function calculateCartTotal(...num1) {
+    return num1
+}
+
+// console.log(calculateCartTotal(100, 200, 400, 5000));
+
+
+const user = {
+    username: "Jay",
+    pass: "Jay@1234"
+}
+
+function handleObject(anyObject) {
+    console.log(`My username is ${anyObject.username} and pass is ${anyObject.pass}.`);    
+}
+
+// handleObject(user);
+
+handleObject({
+    username: "hello",
+    pass: "helloo123"
+})
+
+const myNewArray = [100, 200, 300, 400]
+function handleArray(params) {
+    return params[1]
+}
+
+// console.log(handleArray(myNewArray))
+console.log(handleArray([400, 300, 200, 100]));
