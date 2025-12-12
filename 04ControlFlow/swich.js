465e5dc9d239e7cbd0ff4dfa05c43514b52fe0b7
Jay Gandhi <Jaygandhi813@gmail.com>
2025-12-12 12:48:30 +0530

    add controlFlow folder

---
+++ 04ControlFlow/swich.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month = 8;
+
+switch (month) {
+    case 1:
+        console.log(`jan`);
+        break;
+    case 2:
+        console.log(`feb`);
+        break;
+    case 3:
+        console.log(`mac`);
+        break;
+    case 4:
+        console.log(`apr`);
+        break;
+    case 5:
+        console.log(`may`);
+        break;
+    case 6:
+        console.log(`jun`);
+        break;
+    case 7:
+        console.log(`jly`);
+        break;
+    case 8:
+        console.log(`aug`);
+        break;
+    case 9:
+        console.log(`sept`);
+        break;
+    case 10:
+        console.log(`oct`);
+        break;
+    case 11:
+        console.log(`nuv`);
+        break;
+    case 12:
+        console.log(`dec`);
+        break;
+
+    default:
+        console.log(`enter 1 to 12`);
+        break;
+}
